c5c47a446a3cf8c474eee1b27f2d345e572e544c
Daniel-Constantin Mierla <miconda@gmail.com>
2024-06-09 08:41:23 +0200

    sip-registers.lua: script to list details of REGISTER transactions

---
+++ sip-registers.lua
@@ -0,0 +1,94 @@
+-- List REGISTER transactions
+-- * details for first received REGISTER
+-- * details for last response sent to the origin of the REGISTER
+-- * the key is 'Call-Id::CSeq-Number'
+
+print("-- starting")
+
+do
+	sipregs = {}
+	local sip_request_line_f = Field.new("sip.Request-Line")
+	local sip_status_code_f = Field.new("sip.Status-Code")
+	local sip_call_id_f = Field.new("sip.Call-ID")
+	local sip_cseq_method_f = Field.new("sip.CSeq.method")
+	local sip_cseq_number_f = Field.new("sip.CSeq.seq")
+	local sip_expires_f = Field.new("sip.Expires")
+	local sip_to_user_f = Field.new("sip.to.user")
+
+	local function register_listener()
+		local tap = Listener.new(nil, "(sip.CSeq.method == REGISTER)")
+		function tap.packet(pinfo, tvb, tapinfo)
+			local sip_request_line = tostring(sip_request_line_f() or "none")
+			local sip_status_code = tostring(sip_status_code_f() or "none")
+			local sip_call_id = tostring(sip_call_id_f())
+			local sip_cseq_method = tostring(sip_cseq_method_f())
+			local src_addr = tostring(pinfo.src) .. ":" .. tostring(pinfo.src_port)
+			local dst_addr = tostring(pinfo.dst) .. ":" .. tostring(pinfo.dst_port)
+			local sip_cseq_number = tostring(sip_cseq_number_f())
+			local sip_expires = tonumber(tostring(sip_expires_f() or "-1"))
+			local sip_to_user = tostring(sip_to_user_f())
+			local regkey = sip_call_id .. "::" .. sip_cseq_number
+
+			if sip_request_line ~= "none" then
+				if sipregs[regkey] == nil then
+					sipregs[regkey] = {}
+				end
+				if sipregs[regkey]["REGISTER_FRAMENO"] == nil then
+					sipregs[regkey]["REGISTER_FRAMENO"] = pinfo.number
+					sipregs[regkey]["REGISTER_TIME"] = pinfo.abs_ts
+					sipregs[regkey]["REGISTER_SRC"] = src_addr
+					sipregs[regkey]["REGISTER_DST"] = dst_addr
+					sipregs[regkey]["REGISTER_EXPIRES"] = sip_expires
+					sipregs[regkey]["REGISTER_TOUSER"] = sip_to_user
+				end
+			end
+			if sip_status_code ~= "none" and sipregs[regkey] ~= nil
+					and sipregs[regkey]["REGISTER_SRC"] == dst_addr then
+				sipregs[regkey]["REPLY_FRAMENO"] = pinfo.number
+				sipregs[regkey]["RREPLY_CODE"] = sip_status_code
+				sipregs[regkey]["RREPLY_TIME"] = pinfo.abs_ts
+				sipregs[regkey]["REPLY_SRC"] = src_addr
+				sipregs[regkey]["REPLY_DST"] = dst_addr
+			end
+		end
+		function print_j(tbl, indent)
+			indent = indent or '    '
+			io.write("{\n")
+			local l = 0
+			local t = 0
+			for k, v in pairs(tbl) do
+				if l == 1 then
+					io.write(",\n")
+				end
+				io.write(indent .. string.format("%q", k) .. ": ")
+				if type(v) == "table" then
+					print_j(v, indent .. '    ')
+					io.write("\n")
+					t = 1
+				else
+					if type(v) == "number" then
+						io.write(tostring(v))
+					else
+						io.write(string.format("%q", tostring(v)))
+					end
+					t = 0
+				end
+				l = 1
+			end
+			if t == 0 then
+				io.write("\n")
+			end
+			io.write(string.sub(indent, 1, -5) .. "}")
+		end
+		function tap.draw()
+			print("-- ready")
+			print_j(sipregs)
+			print()
+			print("-- done")
+		end
+		function tap.reset()
+			print("-- finished")
+		end
+	end
+	register_listener()
+end
